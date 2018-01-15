9567fa511096bdc84ab6f67ea31cedb80864b1d8
Agustin Sarasua <agustin.sarasua@mercadolibre.com>
2018-01-15 20:08:40 -0300

    fixes

--- app/catalog_web.go
+++ app/catalog_web.go
@@ -52,8 +52,8 @@ func GetCatalogCountryPropertyTypesEndpoint(w http.ResponseWriter, req *http.Req
 
 func GetCatalogCountryAmenitiesEndpoint(w http.ResponseWriter, req *http.Request) {
 	country, _ := mux.Vars(req)["country"]
-	if v, err := GetCatalogCountryPropertyTypes(country); err != nil {
-		log.Printf("Error getting propertyTypes")
+	if v, err := GetCatalogCountryAmenities(country); err != nil {
+		log.Printf("Error getting amenities")
 		w.WriteHeader(http.StatusBadRequest)
 		json.NewEncoder(w).Encode(m.BuildErrorResponse([]error{err}))
 	} else {
